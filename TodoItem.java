c8a93ba7e17ae01ef97adacf3ed99d926a5bf1c8
monzag <zaganiacz.m@gmail.com>
2017-08-25 11:19:10 +0200

    Add TodoItem class

--- TodoItem.java
+++ TodoItem.java
@@ -4,4 +4,33 @@
  */
 public class TodoItem{
 
+    private String name;
+    private Boolean isDone;
+
+    public TodoItem(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+
+    public void setIsDone() {
+        this.isDone = true;
+    }
+
+    public String displayTodoItem() {
+        String unmark = "[ ] ";
+        String mark = "[X] ";
+        String info;
+
+        if (isDone == true) {
+            info = mark + name;
+        } else {
+            info = unmark + name;
+        }
+
+        return info;
+    }
+
+    // equal method??
+    
+    
 }
