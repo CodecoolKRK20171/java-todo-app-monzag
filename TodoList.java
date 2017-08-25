62dbac2f0546f53e672625561d2fd427dae014a0
monzag <zaganiacz.m@gmail.com>
2017-08-25 12:35:02 +0200

    Add TodoList class

--- TodoList.java
+++ TodoList.java
@@ -1,7 +1,34 @@
+import java.util.ArrayList;
+
 /**
  * Class representing a todo list.
  * TodoList object should aggregate TodoItem objects.
  */
 public class TodoList{
+
+    private ArrayList<TodoItem> listOfItems;
+
+    public TodoList() {
+        listOfItems = new ArrayList<>();
+        this.listOfItems = listOfItems;
+    }
+
+    public void addItem(TodoItem item) {
+        listOfItems.add(item);
+    }
+
+    public void markItem(Integer index) {
+        TodoItem item = listOfItems.get(index);
+         item.setIsDone();
+     }
+
+    public void archive() {
+        for (TodoItem item : listOfItems) {
+            if (item.getIsDone() == true) {
+                listOfItems.remove(item);
+            }
+        }
+    }
+
 
 }
